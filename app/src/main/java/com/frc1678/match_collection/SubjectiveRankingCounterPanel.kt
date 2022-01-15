6170a5b0e6b6e2e5b1d25be67ba7000af10b048d
Kate U <kmoomie123@gmail.com>
2022-01-14 19:17:08 -0800

    push for Alison

--- app/src/main/java/com/frc1678/match_collection/SubjectiveRankingCounterPanel.kt
+++ app/src/main/java/com/frc1678/match_collection/SubjectiveRankingCounterPanel.kt
@@ -2,10 +2,12 @@
 package com.frc1678.match_collection
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.subjective_ranking_counter_panel.*
 
@@ -37,8 +39,8 @@ class SubjectiveRankingCounterPanel : Fragment() {
     // Retrieve a HashMap of inputted data of a subjective team counter panel.
     fun getRankingData(): HashMap<String, Int> {
         val rankingData = HashMap<String, Int>()
-        val rootLayout = view as LinearLayout
-        var counter: SubjectiveRankingCounter
+        val rootLayout = panel_linear_layout
+        var counter: SubjectiveRankingCounter //null
 
         for (i in 0 until (view as LinearLayout).childCount - 1) {
             counter = rootLayout.getChildAt(i + 1) as SubjectiveRankingCounter
