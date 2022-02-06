d08e375571bd1722896e798c8fa8ad7c5caae6fb
Cyan <blueplus.d@gmail.com>
2022-02-06 10:01:20 -0800

    scout id error popup

--- app/src/main/java/com/frc1678/match_collection/MatchInformationInputActivity.kt
+++ app/src/main/java/com/frc1678/match_collection/MatchInformationInputActivity.kt
@@ -132,6 +132,11 @@ class MatchInformationInputActivity : MatchInformationActivity() {
 
             AlertDialog.Builder(this).setMessage(R.string.error_schedule_not_found).show()
         }
+
+        if (btn_scout_id.text == "Scout ID: NONE") {
+
+          AlertDialog.Builder(this).setMessage(R.string.error_scout_id_not_found).show()
+      }
     }
 
     // Assign team number based on collection mode when match number is changed.
